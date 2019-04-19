b44237121e3e507d8b98108e05f6d46f2073ec1e
Hunter Carlisle <huntingcarlisle@gmail.com>
2019-04-19 00:38:29 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,9 @@
 class Helpers
+  def self.current_user(session_hash)
+    @user = User.find(session[:user_id])
+  end
+
+  def self.is_logged_in?(session_hash)
+    return !!session[:user_id]
+  end
 end
