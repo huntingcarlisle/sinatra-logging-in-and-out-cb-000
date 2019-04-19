6c341b689617c868c9747d1b819e1e03a276a985
Hunter Carlisle <huntingcarlisle@gmail.com>
2019-04-19 00:26:39 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20190419002230) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password"
+    t.integer "balance"
+  end
 
 end
